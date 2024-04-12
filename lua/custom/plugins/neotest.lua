1e157737d4dd88cd98431d62b63ea6c7e26f4509
Denilson <denilson020898@gmail.com>
2024-04-12 15:05:16 +0700

    fix neotest

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,9 +1,10 @@
 return {
   'nvim-neotest/neotest',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-lua/plenary.nvim',
-    'antoinemadec/FixCursorHold.nvim',
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
 
     'nvim-neotest/neotest-python',
     'nvim-neotest/neotest-plenary',
