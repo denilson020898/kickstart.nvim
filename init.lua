8dd5e2871198a628a06096a5b093d06585627050
denilson <denilson020898@gmail.com>
2024-09-09 23:44:30 +0700

    update

--- init.lua
+++ init.lua
@@ -629,7 +629,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        ts_ls = {},
 
         -- zls = {
         --   cmd = { '/home/son/.zvm/bin/zls' },
@@ -668,7 +668,7 @@ require('lazy').setup({
         'pyright',
         'jq',
         'sqlls',
-        'tsserver',
+        -- 'tsserver',
 
         'lemminx',
       })
